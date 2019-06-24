0c9b5cf2531ec04dd0903a050b5fb872667cc303
Aidan Heller <asfheller@gmail.com>
2019-06-23 17:27:03 -0700

    Add update check

--- src/main/kotlin/org/sert2521/minisegwayds/ui/UI.kt
+++ src/main/kotlin/org/sert2521/minisegwayds/ui/UI.kt
@@ -3,9 +3,14 @@ package org.sert2521.minisegwayds.ui
 import processing.core.PApplet
 
 class UI<T : Box>(val root: T) {
+    var update = true
+
     fun render(applet: PApplet) {
-        applet.clear()
-        root.render(applet, root.marginLeft, root.marginTop)
+        if (update) {
+            applet.clear()
+            root.render(applet, root.marginLeft, root.marginTop)
+            update = false
+        }
     }
 }
 
